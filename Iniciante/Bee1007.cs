5c795ad658bfe540fbfb403b71467e20b56cbb98
AngelAnds <96259785+AngelAnds@users.noreply.github.com>
2024-09-14 15:20:40 -0400

    Create Bee1007.cs

---
+++ Iniciante/Bee1007.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int a, b, c, d, dif;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        c = int.Parse(Console.ReadLine());
+        d = int.Parse(Console.ReadLine());
+        dif = ((a * b) - (c * d));
+        Console.WriteLine("DIFERENCA = " + dif);
+
+    }
+
+}
