497c441981c0a00056180677c641ed38ceba3d23
AngelAnds <96259785+AngelAnds@users.noreply.github.com>
2024-09-14 15:06:47 -0400

    Create Bee1003.cs

---
+++ Iniciante/Bee1003.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int a, b, x;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        x = a + b;
+        Console.WriteLine("SOMA = " + x);
+
+    }
+
+}
