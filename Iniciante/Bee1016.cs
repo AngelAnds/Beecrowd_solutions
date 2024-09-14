24ea9daf8f6df11576ebf81901fa9a8dc917577f
AngelAnds <96259785+AngelAnds@users.noreply.github.com>
2024-09-14 15:44:34 -0400

    Create Bee1016.cs

---
+++ Iniciante/Bee1016.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Area01
+{
+    class Teste01
+    {
+        static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{numero*2} minutos");
+        }
+    }
+}
