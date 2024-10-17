175de774acb9c49254a45e1c1d8c21bd7ed878ce
AngelAnds <96259785+AngelAnds@users.noreply.github.com>
2024-10-16 20:12:41 -0400

    .

---
+++ Iniciante/Bee1059.cs
@@ -0,0 +1,14 @@
+using System;
+namespace URI //@AngelAnds 1059
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            for (int c = 2; c <= 100; c += 2)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
